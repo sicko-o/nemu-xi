40a536bc76c2b4bcc24d3c50ad1fc6a182155825
tracer-ics2016 <tracer@njuics.org>
2020-10-28 11:41:13 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:41:13 up 12 days,  4:13,  3 users,  load average: 0.38, 0.29, 0.17 bbaf9617e16152404995fad06ada5025e4b4a521

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -7,7 +7,7 @@ make_helper(call)
 	swaddr_t ret_addr = cpu.eip + len + 1;
 	swaddr_write(cpu.esp - 4 , 4 , ret_addr);
 	cpu.esp = cpu.esp - 4;
-	cpu.eip = op_src->val;
+	cpu.eip = cpu.eip + op_src->val;
 	print_asm("call %x" , cpu.eip + 1 + len);
 	return len + 1;
 
