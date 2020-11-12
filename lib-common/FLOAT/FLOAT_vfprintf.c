6a6333452c856c7cb22dd53d6e0afe349b56f49f
tracer-ics2016 <tracer@njuics.org>
2020-11-12 15:58:49 +0000

    >  compile kernel 33019244310 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:58:49 up 3 days,  8:01,  4 users,  load average: 0.22, 0.26, 0.25 35d8d4cebf1438e1cee9f6d1642b0c092ab83f93

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -16,8 +16,8 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 	 */
 
 	char buf[80];
-	int len = sprintf(buf, "0x%08x", f);
-	return __stdio_fwrite(buf, len, stream);
+	//int len = sprintf(buf, "0x%08x", f);
+	//return __stdio_fwrite(buf, len, stream);
 }
 
 static void modify_vfprintf() {
